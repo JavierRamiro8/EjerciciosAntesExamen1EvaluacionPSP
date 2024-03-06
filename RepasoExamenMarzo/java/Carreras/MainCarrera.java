b8fecd6ca3f11b8d336d5d7157f49f695799f95c
JavierRamiro8 <jramiroc04@gmail.com>
2024-03-06 21:52:24 +0100

    lock

--- RepasoExamenMarzo/java/Carreras/MainCarrera.java
+++ RepasoExamenMarzo/java/Carreras/MainCarrera.java
@@ -1,28 +1,29 @@
 package RepasoExamenMarzo.java.Carreras;
 
 public class MainCarrera {
-    private static Object lock=new Object();
+    private static Object lock = new Object();
+
     public static void main(String[] args) {
-        Thread[] hilo=new Thread[20];
-        Carrera[] corredor=new Carrera[20];
-        for(int i=0;i<corredor.length;i++){
-            int dorsal=(int)(Math.random()*100);
-            corredor[i]=new Carrera(dorsal);
-            hilo[i]=new Thread(corredor[i]);
+        Thread[] hilo = new Thread[20];
+        Carrera[] corredor = new Carrera[20];
+        for (int i = 0; i < corredor.length; i++) {
+            int dorsal = (int) (Math.random() * 100);
+            corredor[i] = new Carrera(dorsal);
+            hilo[i] = new Thread(corredor[i]);
         }
-        synchronized(lock){
-            for(int i=0;i<hilo.length;i++){
+        synchronized (lock) {
+            for (int i = 0; i < hilo.length; i++) {
+                hilo[i].start();
                 try {
-                    hilo[i].start();
-                    hilo[i].wait();
+                    lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
-        hilo.notifyAll();
+        lock.notifyAll();
 
-        for(int i=0;i<hilo.length;i++){
+        for (int i = 0; i < hilo.length; i++) {
             try {
                 hilo[i].join();
             } catch (InterruptedException e) {
