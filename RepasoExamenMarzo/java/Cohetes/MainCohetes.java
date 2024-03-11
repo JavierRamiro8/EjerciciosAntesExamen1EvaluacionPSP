a8936960e53bde87be9ee6580cd8c86a935d3c38
JavierRamiro8 <jramiroc04@gmail.com>
2024-03-11 21:56:22 +0100

    argumentos cohetes

--- RepasoExamenMarzo/java/Cohetes/MainCohetes.java
+++ RepasoExamenMarzo/java/Cohetes/MainCohetes.java
@@ -1,34 +1,39 @@
 package RepasoExamenMarzo.java.Cohetes;
 
-/*Integer.parseInt(args[0]); */
+/* */
 public class MainCohetes {
     public static Object lock = new Object();
 
     public static void main(String[] args) {
-        int numeroCohetes = 4;
-        Cohete cohetes[] = new Cohete[numeroCohetes];
-        Thread coheteHilos[] = new Thread[numeroCohetes];
-        for (int i = 0; i < numeroCohetes; i++) {
-            cohetes[i] = new Cohete();
-            coheteHilos[i] = new Thread(cohetes[i]);
-        }
-        for (int i = 0; i < numeroCohetes; i++) {
-            coheteHilos[i].start();
-        }
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        synchronized (lock) {
-            lock.notifyAll();
-        }
-        for (int i = 0; i < numeroCohetes; i++) {
-            try {
-                coheteHilos[i].join();
-            } catch (InterruptedException e) {
+        int numeroCohetes = Integer.parseInt(args[0]);
+        if (numeroCohetes == 0 || numeroCohetes < 0) {
+            System.out.println("Argumentos insuficientes");
+        } else {
+            Cohete cohetes[] = new Cohete[numeroCohetes];
+            Thread coheteHilos[] = new Thread[numeroCohetes];
+            for (int i = 0; i < numeroCohetes; i++) {
+                cohetes[i] = new Cohete();
+                coheteHilos[i] = new Thread(cohetes[i]);
             }
+            for (int i = 0; i < numeroCohetes; i++) {
+                coheteHilos[i].start();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lock) {
+                lock.notifyAll();
+            }
+            for (int i = 0; i < numeroCohetes; i++) {
+                try {
+                    coheteHilos[i].join();
+                } catch (InterruptedException e) {
+                }
+            }
+            Cohete.estadoMision();
         }
-        Cohete.estadoMision();
+
     }
 }
